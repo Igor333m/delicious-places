1c9d08dc03ddfd87cfac275d8534c738c2d45cab
Igor Todorovic <igor333m@yahoo.com>
2020-08-28 16:17:29 -0700

    Remove connect-flashes package

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
 const promisify = require('es6-promisify');
-const flash = require('connect-flash');
 const expressValidator = require('express-validator');
 const routes = require('./routes/index');
 const helpers = require('./helpers');
@@ -37,7 +36,7 @@ app.use(expressValidator());
 app.use(cookieParser());
 
 // Sessions allow us to store data on visitors from request to request
-// This keeps users logged in and allows us to send flash messages
+// This keeps users logged in
 app.use(session({
   secret: process.env.SECRET,
   key: process.env.KEY,
@@ -50,13 +49,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
-app.use(flash());
-
 // pass variables to our templates + all requests
 app.use((req, res, next) => {
   res.locals.h = helpers;
-  res.locals.flashes = req.flash();
   res.locals.user = req.user || null;
   res.locals.currentPath = req.path;
   next();
@@ -73,9 +68,6 @@ app.use('/', routes);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
-
-// One of our error handlers will see if these errors are just validation errors
-app.use(errorHandlers.flashValidationErrors);
 
 // Otherwise this was a really bad error we didn't expect! Shoot eh
 if (app.get('env') === 'development') {
